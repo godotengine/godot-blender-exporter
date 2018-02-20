8921567b092314cbeecc74b47d6c21ee20f28bb5
vikasTmz <vikas.thamizharasan@students.iiit.ac.in>
2018-02-20 22:02:27 +0530

    indentation error

--- io_scene_godot/build.py
+++ io_scene_godot/build.py
@@ -7,8 +7,8 @@ SRC_DIR = dirname(abspath(__file__))
 
 with zipfile.ZipFile('io_scene_godot.zip', 'w', zipfile.ZIP_DEFLATED) as arch:
     for filename in [
-            '__init__.py',
-			'export_godot.py']:
+		'__init__.py',
+		'export_godot.py']:
         arch.write(pjoin(SRC_DIR, filename), 'io_scene_godot/'+filename)
 
 print('created file: io_scene_godot.zip')
