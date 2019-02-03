c5c0bffd4198d8ec45315941bc61a1a64d25b5d6
Jason0214 <lujc.me@gmail.com>
2019-02-03 15:57:24 -0800

    rewrite constraint baking

--- io_scene_godot/converters/animation/animation_data.py
+++ io_scene_godot/converters/animation/animation_data.py
@@ -2,19 +2,16 @@
 AnimationPlayer as well as distribute Blender action into various
 action exporting functions"""
 
-import bpy
 from .action import (
     export_camera_action,
     export_shapekey_action,
     export_light_action,
+    export_constrained_xform_action,
     export_transform_action
 )
 from .constraint_baking import (
-    bake_constraint_to_action,
     check_object_constraint,
     check_pose_constraint,
-    action_baking_finalize,
-    action_baking_initialize
 )
 from .serializer import get_animation_player
 from .action import ActionStrip
@@ -35,7 +32,6 @@ class ObjectAnimationExporter:
         self.godot_node = godot_node
         self.blender_object = blender_object
 
-        self.action_exporter_func = ACTION_EXPORTER_MAP[action_type]
         self.animation_player = None
 
         self.need_baking = False
@@ -45,6 +41,11 @@ class ObjectAnimationExporter:
 
         self.check_baking_condition(action_type)
         self.preprocess_nla_tracks(blender_object)
+
+        if not self.need_baking:
+            self.action_exporter_func = ACTION_EXPORTER_MAP[action_type]
+        else:
+            self.action_exporter_func = export_constrained_xform_action
 
     def check_baking_condition(self, action_type):
         """Check whether the animated object has any constraint and
@@ -68,44 +69,31 @@ class ObjectAnimationExporter:
                 else:
                     self.mute_nla_tracks.append(nla_track)
 
-    def bake_to_new_action(self, action_to_bake):
-        """Baking object and pose constraint altogether.
-
-        Note that it accept a action to bake (which would not be modified)
-        and always return a new created baked actiony"""
-        return bake_constraint_to_action(
-            self.blender_object, action_to_bake, False
-        )
-
     def export_active_action(self, escn_file, active_action):
         """Export the active action, if needed, would call bake.
-
         Note that active_action maybe None, which would happen when object has
         some constraint (so even no action it is still animated)"""
-        if self.need_baking:
-            action_baking_initialize(active_action)
-            action_active_to_export = self.bake_to_new_action(active_action)
+        if active_action is None:
+            # object has constraints on other objects
+            assert self.need_baking
+            anim_rsc_name = self.blender_object.name + 'Action'
         else:
-            action_active_to_export = active_action
+            anim_rsc_name = active_action.name
 
         if self.animation_player.active_animation is None:
             self.animation_player.add_active_animation_resource(
-                escn_file, action_active_to_export.name
+                escn_file, anim_rsc_name
             )
 
         self.action_exporter_func(
             self.godot_node,
             self.animation_player,
             self.blender_object,
-            ActionStrip(action_active_to_export),
+            ActionStrip(active_action),
             self.animation_player.active_animation
         )
 
-        if self.need_baking:
-            # remove new created action
-            bpy.data.actions.remove(action_active_to_export)
-            action_baking_finalize(active_action)
-        else:
+        if not self.need_baking:
             # here export unmuted nla_tracks into animation resource,
             # this is not needed for baking, as baking has applied to
             # active action
@@ -142,7 +130,6 @@ class ObjectAnimationExporter:
     def export_stashed_track(self, escn_file, stashed_track):
         """Export a muted nla_track, track with all its contained action
         is exported to a single animation_resource.
-
         It works as an action lib"""
         if not stashed_track.strips:
             return
@@ -158,28 +145,22 @@ class ObjectAnimationExporter:
             escn_file, anim_name
         )
 
+        if self.need_baking:
+            stashed_track.mute = False
+
         for strip in stashed_track.strips:
             if strip.action:
-                if self.need_baking:
-                    action_baking_initialize(strip.action)
-                    action_to_export = self.bake_to_new_action(strip.action)
-                else:
-                    action_to_export = strip.action
-
                 self.action_exporter_func(
                     self.godot_node,
                     self.animation_player,
                     self.blender_object,
-                    ActionStrip(strip, action_to_export),
+                    ActionStrip(strip),
                     anim_resource
                 )
 
-                if self.need_baking:
-                    # remove baked action
-                    bpy.data.actions.remove(action_to_export)
-                    action_baking_finalize(strip.action)
-
-        if not self.need_baking:
+        if self.need_baking:
+            stashed_track.mute = True
+        else:  # not self.need_baking:
             # if baking, nla_tracks are already baked into strips
             for nla_track in self.unmute_nla_tracks:
                 for strip in nla_track.strips:
@@ -228,8 +209,11 @@ def export_animation_data(escn_file, export_settings, godot_node,
     # export actions in nla_tracks, each exported to seperate
     # animation resources
     if export_settings['use_stashed_action']:
+        if blender_object.animation_data:
+            # clear active action, isolate NLA track
+            blender_object.animation_data.action = None
         for stashed_track in anim_exporter.mute_nla_tracks:
             anim_exporter.export_stashed_track(escn_file, stashed_track)
 
-    if active_action is not None:
+    if blender_object.animation_data is not None:
         blender_object.animation_data.action = active_action
--- io_scene_godot/converters/animation/constraint_baking.py
+++ io_scene_godot/converters/animation/constraint_baking.py
@@ -1,24 +1,5 @@
 """Collection of helper functions to baking constraints into action"""
-
 import bpy
-import bpy_extras.anim_utils
-
-# a suffix append to action need baking to avoid name collision
-# with baked action's name
-BAKING_SUFFIX = '--being-baking'
-
-
-def action_baking_initialize(action):
-    """Intialize steps before an action going to baking"""
-    if action is not None:
-        action.name = action.name + BAKING_SUFFIX
-
-
-def action_baking_finalize(action):
-    """Clear up some baking information for an action having
-    going through baking"""
-    if action is not None:
-        action.name = action.name[:-len(BAKING_SUFFIX)]
 
 
 def check_object_constraint(blender_object):
@@ -36,39 +17,3 @@ def check_pose_constraint(blender_object):
             if pose_bone.constraints:
                 return True
     return False
-
-
-def bake_constraint_to_action(blender_object, base_action, in_place):
-    """Bake pose or object constrainst (e.g. IK) to action"""
-    if base_action is not None:
-        blender_object.animation_data.action = base_action
-        frame_range = range(
-            int(base_action.frame_range[0]),
-            int(base_action.frame_range[1]) + 1)
-    else:
-        frame_range = range(1, 251)  # default, can be improved
-
-    # if action_bake_into is None, it would create a new one
-    # and baked into it
-    if in_place:
-        action_bake_into = base_action
-    else:
-        action_bake_into = None
-
-    baked_action = bpy_extras.anim_utils.bake_action_objects(
-        object_action_pairs=[(blender_object, action_bake_into)],
-        frames=frame_range,
-        only_selected=False,
-        do_pose=True,
-        do_object=True,
-        do_visual_keying=True,
-    )[0]
-
-    if in_place:
-        return action_bake_into
-
-    if base_action is not None:
-        baked_action.name = base_action.name[:-len(BAKING_SUFFIX)]
-    else:
-        baked_action.name = blender_object.name + 'Action'
-    return baked_action
