238fea857b8ac503183218fac885d19c2054fbbe
Anders Stenberg <anders.stenberg@gmail.com>
2019-08-24 16:56:05 +0200

    Fix dodgy parentheses

--- io_scene_godot/converters/material/script_shader/node_converters.py
+++ io_scene_godot/converters/material/script_shader/node_converters.py
@@ -760,7 +760,7 @@ class ImageTextureNodeConverter(NodeConverterBase):
         tex_coord_socket = self.bl_node.inputs[0]
         tex_coord = self.in_sockets_map[tex_coord_socket]
         if not tex_coord_socket.is_linked:
-            self.local_code.append("%s = vec3（UV, 0.0）" % tex_coord)
+            self.local_code.append("%s = vec3(UV, 0.0)" % tex_coord)
 
         tex_var = self.generate_tmp_texture_id(self.bl_node.name)
         if self.bl_node.image is not None:
