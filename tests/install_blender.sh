45cda03c63e4c689f25ccb0f95887e5bd12e059e
Willian Galvani <williangalvani@gmail.com>
2020-10-20 22:12:01 -0300

    Create  GitHub action for 2.90.1

--- tests/install_blender.sh
+++ tests/install_blender.sh
@@ -1,23 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 BLENDER_VERSION=$1
-
-BLENDER_LINUX_64_URL="https://mirror.clarkson.edu/blender/release/Blender${BLENDER_VERSION}/blender-${BLENDER_VERSION}-linux-glibc217-x86_64.tar.bz2"
-
-BLENDER_DIRNAME_REGEX_PATTERN='/Blender2\.[0-9]+/(.+)\.tar\.bz2$'
+# https://download.blender.org/source/blender-2.90.1.tar.xz
+BLENDER_LINUX_64_URL="https://download.blender.org/release/Blender2.90/blender-2.90.1-linux64.tar.xz"
+BLENDER_DIRNAME_REGEX_PATTERN='/blender-2\.[0-9]+(\.[0-9]+)?-(.+)\.tar\.xz$'
 [[ ${BLENDER_LINUX_64_URL} =~ ${BLENDER_DIRNAME_REGEX_PATTERN} ]]
-NAME=${BASH_REMATCH[1]}
+echo $BLENDER_DIRNAME_REGEX_PATTERN
+NAME=${BASH_REMATCH[0]%".tar.xz"}
 
 CACHE="${HOME}/.blender-cache"
-TAR="${CACHE}/${NAME}.tar.bz2"
+TAR="${CACHE}/${NAME}.tar.xz"
 
 echo "Installing Blender ${BLENDER_VERSION}"
 mkdir -p $CACHE
 if [ ! -f $TAR ]; then
     wget -O $TAR $BLENDER_LINUX_64_URL
 fi
-tar -xjf $TAR -C $HOME
+tar -xvf $TAR -C $HOME
 
 echo "export BLENDER_BIN=\"${HOME}/${NAME}/blender\"" > .envs
